de77fe0c4c5400454d5b50db0f979cbc5d8f3961
Christian Martin Roffey <81240124+christianmartinroffey@users.noreply.github.com>
2023-11-22 22:25:24 +0100

    Develop > master (#2)
    * rework squatChecker and remove athlete orientation
    
    * reworks maths logic to not divide by 0
    
    * add logic to determine which angels to use based on visibility value

--- modules/PoseModule.py
+++ modules/PoseModule.py
@@ -45,16 +45,17 @@ class PoseDetector:
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                self.lmList.append([id, cx, cy])
+                visibility = lm.visibility
+                self.lmList.append([id, cx, cy, visibility])
                 if draw:
                     cv2.circle(img, (cx, cy), 3, (255, 0, 0), cv2.FILLED)
         return self.lmList
 
     def getAngle(self, img, p1, p2, p3, draw=True):
         # Get the points
-        x1, y1 = self.lmList[p1][1:]
-        x2, y2 = self.lmList[p2][1:]
-        x3, y3 = self.lmList[p3][1:]
+        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
+        x3, y3 = self.lmList[p3][1], self.lmList[p3][2]
 
         # Calculate vectors
         v1 = (x1 - x2, y1 - y2)
@@ -65,8 +66,14 @@ class PoseDetector:
         mag1 = math.sqrt(v1[0] ** 2 + v1[1] ** 2)
         mag2 = math.sqrt(v2[0] ** 2 + v2[1] ** 2)
 
+        if mag1 * mag2 == 0:
+            return 0
+
+            # Clamp value between -1 and 1
+        cosine_angle = max(min(dot / (mag1 * mag2), 1), -1)
+
         # Calculate the angle in radians and then convert to degrees
-        angle = math.degrees(math.acos(dot / (mag1 * mag2)))
+        angle = math.degrees(math.acos(cosine_angle))
 
         if draw:
             # draws white line from points for visibility
