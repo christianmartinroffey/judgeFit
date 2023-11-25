f1c68d98145ab8f1c1270ed21a079bc1856b76b3
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2023-11-25 19:10:45 +0100

    rework squatchecker logic to function with direction, squat depth and extention correctly assigning no reps

--- modules/squatChecker.py
+++ modules/squatChecker.py
@@ -5,26 +5,29 @@ import math
 import mediapipe as mp
 import PoseModule as pm
 
-
 # section to use a video
 mpPose = mp.solutions.pose
 mpDraw = mp.solutions.drawing_utils
 pose = mpPose.Pose()
 
-video = cv2.VideoCapture('../videos/airsquat.mp4')
+video = cv2.VideoCapture('../videos/luisprsquat.mp4')
 pTime = 0
 
-
 detector = pm.PoseDetector()
+
 count = 0
 no_rep = 0
 # 1 = up, 0 = down
-direction = 0
+direction = None
 start_point = 0
 end_point = 0
 is_squat_started = False
 full_depth = False
+full_extension = False
 outcome = ""
+descending_threshold = 120  # Threshold to indicate start of squat
+ascending_threshold = 120
+
 
 while True:
     success, img = video.read()
@@ -51,63 +54,77 @@ while True:
         angle = detector.getAngle(img, hip_index, knee_index, ankle_index)
 
         # Update start and end points for the new angle range
-        start_point = 170  # Example value, adjust as needed
-        end_point = 45     # Example value, adjust as needed
+        start_point = 170  # extended value
+        end_point = 45  # full range when reached
         percentage = int(round(np.interp(angle, (end_point, start_point), (100, 0))))
 
         # Squat analysis logic
-        if percentage == 100 and not is_squat_started:
-            is_squat_started = True
-            full_depth = False
+        if angle > descending_threshold:
+            direction = 1  # Up
+        elif angle < ascending_threshold:
+            direction = 0  # Down
 
-        if is_squat_started and direction == 0:
+            # Squat analysis logic
+        if direction == 0 and angle < descending_threshold:
+            if not is_squat_started:
+                is_squat_started = True
+                full_depth = False
+                full_extension = False
+
             if angle <= end_point:
                 full_depth = True
-                direction = 1
-            else:
-                full_depth = False
 
-        if full_depth and direction == 1:
+        elif direction == 1 and is_squat_started:
             if angle >= start_point:
-                count += 1
-                is_squat_started = False
-                direction = 0
-                full_depth = False
-                outcome = "good rep"
-                # TODO fix no lockout logic
-            elif angle <= start_point:
-                is_squat_started = False
-                direction = 0
-                full_depth = False
-                no_rep += 1
-                outcome = "not extended"
-        elif not full_depth and is_squat_started:
-            if angle >= start_point:
-                no_rep += 1
-                outcome = "not deep enough"
-                is_squat_started = False
-                direction = 0
+                full_extension = True
 
+            # Check for no full extension
+            # if angle is going higher it means athlete is going up
+            if full_depth:
+                if not full_extension:
+                    if angle < previous_angle:
+                        no_rep += 1
+                        outcome = "no full extension"
+                        direction = 0
+                        is_squat_started = False
+            elif not full_depth:
+                # if didn't reach full depth and they start going up
+                if angle > previous_angle and full_extension:
+                    no_rep += 1
+                    outcome = "not deep enough"
+                    direction = 1
+                    is_squat_started = False
+
+            # # Reset variables for next rep
+            # is_squat_started = False
+            # full_depth = False
+            # full_extension = False
+
+        if full_depth and full_extension:
+            count += 1
+            outcome = "good rep"
+            # Reset variables for next rep
+            is_squat_started = False
+            full_depth = False
+            full_extension = False
+
+        # change to int in case there are minor differences in float values
+        previous_angle = int(angle)
 
         # Output for debugging or display
         print(
-            angle,
-            "%", percentage,
-            "start_squat", is_squat_started,
+            int(angle),
+            direction,
+            "extension", full_extension,
             "full depth", full_depth,
             "reps", count,
             "no reps", no_rep
         )
-        # start at top i.e 100% and direction is 0 i.e going down
-        if percentage == 100 and not is_squat_started:
-            is_squat_started = True
-            full_depth = False
 
-
-        cv2.putText(img, f'reps: {int(count)}', (50, 100), cv2.FONT_HERSHEY_PLAIN, 7, (255,0,0), 8)
-        cv2.putText(img, f'{int(percentage)} %', (50, 300), cv2.FONT_HERSHEY_PLAIN, 7, (0,0,255), 8)
-        cv2.putText(img, f'no reps: {int(no_rep)}', (50, 500), cv2.FONT_HERSHEY_PLAIN, 7, (0,0,255), 8)
-        cv2.putText(img, f'outcome: {outcome}', (50, 700), cv2.FONT_HERSHEY_PLAIN, 7, (0,0,255), 8)
+        cv2.putText(img, f'reps: {int(count)}', (50, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 5)
+        # cv2.putText(img, f'{int(percentage)} %', (50, 300), cv2.FONT_HERSHEY_PLAIN, 7, (0,0,255), 8)
+        cv2.putText(img, f'no reps: {int(no_rep)}', (500, 100), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 5)
+        cv2.putText(img, f'outcome: {outcome}', (50, 200), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 5)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
