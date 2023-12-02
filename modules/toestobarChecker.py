f9c0b7699e1169ab7c085d3a74b9b11c657cc8e4
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2023-12-02 11:26:41 +0100

    rework logic to use angles for ttb

--- modules/toestobarChecker.py
+++ modules/toestobarChecker.py
@@ -10,7 +10,7 @@ mpPose = mp.solutions.pose
 mpDraw = mp.solutions.drawing_utils
 pose = mpPose.Pose()
 
-video = cv2.VideoCapture('../videos/airsquat.mp4')
+video = cv2.VideoCapture('../videos/toestobar.MOV')
 pTime = 0
 
 detector = pm.PoseDetector()
@@ -19,15 +19,15 @@ count = 0
 no_rep = 0
 # 1 = up, 0 = down
 direction = None
-start_point = 0
-end_point = 0
-is_squat_started = False
-full_depth = False
+start_point = 170
+end_point = 10
+is_movement_started = False
+full_range = False
 full_extension = False
 outcome = ""
-descending_threshold = 110  # Threshold to indicate start of squat
-ascending_threshold = 110
-
+descending_threshold = 30  # Threshold to indicate start of squat
+ascending_threshold = 150
+full_range_threshold = 250  # Adjust threshold based on your requirement
 
 while True:
     success, img = video.read()
@@ -35,90 +35,125 @@ while True:
 
     lmList = detector.getPosition(img, draw=False)
     if len(lmList) != 0:
+        # landmarks for hands and feet to check for full range
+        left_hand_y = min(lmList[19][2], lmList[17][2])
+        right_hand_y = min(lmList[20][2], lmList[18][2])
+        left_toe_y = lmList[31][2]
+        right_toe_y = lmList[32][2]
+
+        # get visibility depending on which way athlete is facing
         left_hip_visibility = lmList[23][3]  # Visibility of left hip (landmark 23)
         right_hip_visibility = lmList[24][3]  # Visibility of right hip (landmark 24)
 
         # Choose landmarks based on hip visibility
         if left_hip_visibility > right_hip_visibility:
-            # Use left side landmarks (23, 25, 27)
+            # Use left side landmarks
+            shoulder_index = 11
             hip_index = 23
-            knee_index = 25
             ankle_index = 27
         else:
-            # Use right side landmarks (24, 26, 28)
+            # Use right side landmarks
+            shoulder_index = 12
             hip_index = 24
-            knee_index = 26
             ankle_index = 28
 
-        # Calculate the angle for the squat analysis
-        angle = detector.getAngle(img, hip_index, knee_index, ankle_index)
+        # Calculate the angle
+        # Need to use the angle to check for start, end of rep and rep started
+        angle = detector.getAngle(img, shoulder_index, hip_index, ankle_index)
 
-        # Update start and end points for the new angle range
-        start_point = 170  # extended value
-        end_point = 60  # full range when reached
         percentage = int(round(np.interp(angle, (end_point, start_point), (100, 0))))
 
-        # Squat analysis logic
-        if angle > descending_threshold:
+        # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # results = pose.process(imgRGB)
+        # landmarks = results.pose_landmarks.landmark
+
+        # # Get coordinates for toes (Landmarks 31 and 32)
+        # left_toe = [landmarks[31].x, landmarks[31].y]
+        # right_toe = [landmarks[32].x, landmarks[32].y]
+        # right_hand = [landmarks[20].x, landmarks[20].y]
+        # left_hand = [landmarks[19].x, landmarks[19].y]
+        # # Convert coordinates to pixel values
+        # h, w, c = img.shape
+        # left_toe_pixel = np.multiply(left_toe, [w, h]).astype(int)
+        # right_toe_pixel = np.multiply(right_toe, [w, h]).astype(int)
+        # left_hand_pixel = np.multiply(left_hand, [w, h]).astype(int)
+        # right_hand_pixel = np.multiply(right_hand, [w, h]).astype(int)
+        # # Draw circles at the toe positions
+        # cv2.circle(img, tuple(left_toe_pixel), 15, (255, 0, 0), -1)  # Blue circle for left toe
+        # cv2.circle(img, tuple(right_toe_pixel), 15, (0, 255, 0), -1)  # Green circle
+        # cv2.circle(img, tuple(left_hand_pixel), 15, (255, 0, 0), -1)  # Blue circle for left hand
+        # cv2.circle(img, tuple(right_hand_pixel), 15, (0, 255, 0), -1)
+
+        # Movement analysis logic
+        if angle > ascending_threshold:
             direction = 1  # Up
-        elif angle < ascending_threshold:
+        elif angle < descending_threshold:
             direction = 0  # Down
 
-            # Squat analysis logic
-        if direction == 0 and angle < descending_threshold:
-            if not is_squat_started:
-                is_squat_started = True
-                full_depth = False
+        # Movement analysis logic
+        if direction == 1 and angle < ascending_threshold:
+            if not is_movement_started:
+                is_movement_started = True
+                full_range = False
                 full_extension = False
                 outcome = ''
 
-            if angle <= end_point:
-                full_depth = True
+        if angle <= 10:
+            full_range = True
 
-        elif direction == 1 and is_squat_started:
+            # METHOD TO USE Y COORDINATES FOR RANGE
+            # if abs(left_hand_y - left_toe_y) < full_range_threshold or abs(
+            #         right_hand_y - right_toe_y) < full_range_threshold:
+            #     full_range = True
+
+            # print("left", left_hand_y, left_toe_y)
+            # print("right", right_hand_y, right_toe_y)
+
+        elif direction == 1 and is_movement_started:
             if angle >= start_point:
                 full_extension = True
 
             # Check for no full extension
             # if angle is going higher it means athlete is going up
-            if full_depth:
+            if full_range:
                 if not full_extension:
                     if angle < previous_angle:
                         no_rep += 1
                         outcome = "no full extension"
-                        direction = 0
-                        is_squat_started = False
-            elif not full_depth:
-                # if didn't reach full depth and they start going up
+                        direction = 1
+                        is_movement_started = False
+            elif not full_range:
+                # if didn't reach full range and they start going down
                 if angle > previous_angle and full_extension:
                     no_rep += 1
-                    outcome = "not deep enough"
-                    direction = 1
-                    is_squat_started = False
+                    outcome = "did not touch the bar"
+                    direction = 0
+                    is_movement_started = False
 
             # # Reset variables for next rep
-            # is_squat_started = False
-            # full_depth = False
-            # full_extension = False
+            is_movement_started = False
+            full_range = False
+            full_extension = False
 
-        if full_depth and full_extension:
+        if full_range and full_extension:
             count += 1
             outcome = "good rep"
             # Reset variables for next rep
-            is_squat_started = False
-            full_depth = False
+            is_movement_started = False
+            full_range = False
             full_extension = False
 
-        # change to int in case there are minor differences in float values
+        # # change to int in case there are minor differences in float values
         previous_angle = int(angle)
 
-        # Output for debugging or display
+        # Output for debugging
         print(
             int(angle),
+            is_movement_started,
             direction,
             outcome,
             "extension", full_extension,
-            "full depth", full_depth,
+            "full range", full_range,
             "reps", count,
             "no reps", no_rep
         )
