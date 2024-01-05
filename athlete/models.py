f46792496c93d2d29731bab3951d6e5c3bcdfe7b
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2024-01-05 19:42:00 +0100

    add beginnings of athlete and country model.

--- athlete/models.py
+++ athlete/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Athlete(models.Model):
+    name = models.CharField(max_length=40)
+    surname = models.CharField(max_length=40)
+    age = models.IntegerField(max_length=3)
+    email = models.EmailField(max_length=100)
+    country = models.ForeignKey("Country", on_delete=models.PROTECT)
+    registered_at = models.DateTimeField(auto_now=True)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=24)
+    code = models.CharField(
+        max_length=4, blank=True, null=True, verbose_name=u'Codigo ISO', unique=True
+    )
+
+    class Meta:
+        verbose_name = "Country"
+        verbose_name_plural = "Countries"
