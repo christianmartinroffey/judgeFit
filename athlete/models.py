4ab8df6629021c1137328e304bdf929c0f0b5a87
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2024-01-08 19:33:31 +0100

    update athlete model, create region and affiliate models

--- athlete/models.py
+++ athlete/models.py
@@ -2,20 +2,62 @@ from django.db import models
 
 
 class Athlete(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+    GENDER_CHOICES = (
+        MALE, 'Male',
+        FEMALE, 'Female',
+        OTHER, 'Other'
+    )
     name = models.CharField(max_length=40)
     surname = models.CharField(max_length=40)
-    age = models.IntegerField(max_length=3)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=2)
+    date_of_birth = models.DateField()
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
     email = models.EmailField(max_length=100)
     country = models.ForeignKey("Country", on_delete=models.PROTECT)
-    registered_at = models.DateTimeField(auto_now=True)
+    region = models.ForeignKey('Region', on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
+    affiliate = models.ForeignKey("Affiliate", on_delete=models.PROTECT)
+    profile_photo = models.ImageField(upload_to='static/images/')
+    emergency_contact_name = models.CharField(max_length=100)
+    emergency_contact_phone = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "Athlete"
+        verbose_name_plural = "Athletes"
+
+
+class Affiliate(models.Model):
+    name = models.CharField(max_length=40)
+    country = models.ForeignKey("Country", on_delete=models.PROTECT)
+    region = models.ForeignKey('Region', on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Affiliate"
+        verbose_name_plural = "Affiliates"
 
 
 class Country(models.Model):
     name = models.CharField(max_length=24)
     code = models.CharField(
-        max_length=4, blank=True, null=True, verbose_name=u'Codigo ISO', unique=True
+        max_length=4, blank=True, null=True, verbose_name=u'ISO Code', unique=True
     )
 
     class Meta:
         verbose_name = "Country"
         verbose_name_plural = "Countries"
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=24)
+    code = models.CharField(
+        max_length=4, blank=True, null=True, verbose_name=u'ISO Code', unique=True
+    )
+
+    class Meta:
+        verbose_name = "Region"
+        verbose_name_plural = "Regions"
