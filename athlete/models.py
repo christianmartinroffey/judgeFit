81ca473af69b0627ed3849365edac4e4b04c799d
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2024-02-03 18:12:25 +0100

    rework affiliate model, add migrations files for workout and create addaffiliatedata command

--- athlete/models.py
+++ athlete/models.py
@@ -37,12 +37,11 @@ class Affiliate(models.Model):
     postal_code = models.CharField(max_length=10)
     website = models.URLField(max_length=100)
     country = models.ForeignKey("Country", on_delete=models.PROTECT)
-    region = models.ForeignKey('Region', on_delete=models.PROTECT)
     state = models.CharField(max_length=40)
-    photo = models.ImageField(upload_to='static/images/affiliate_images')
     crossfit_affiliate = models.BooleanField(default=False)
     crossfit_affiliate_since = models.DateField()
     created_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='static/images/affiliate_images')
 
     class Meta:
         verbose_name = "Affiliate"
@@ -58,14 +57,3 @@ class Country(models.Model):
     class Meta:
         verbose_name = "Country"
         verbose_name_plural = "Countries"
-
-
-class Region(models.Model):
-    name = models.CharField(max_length=24)
-    code = models.CharField(
-        max_length=4, blank=True, null=True, verbose_name=u'ISO Code', unique=True
-    )
-
-    class Meta:
-        verbose_name = "Region"
-        verbose_name_plural = "Regions"
