2b4645ad39fa7358ccb2d0654ca2049ef72aaf4d
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2024-02-03 19:29:20 +0100

    add country and affiliate to admin

--- athlete/admin.py
+++ athlete/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from athlete.models import Country, Affiliate
+
+
 # Register your models here.
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'router_code', 'code')
+    list_filter = ('name', 'router_code', 'code')
+    search_fields = ('name', 'router_code', 'code')
+
+
+admin.site.register(Country, CountryAdmin)
+
+
+class AffiliateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'country', 'crossfit_affiliate', 'created_at')
+    list_filter = ('name', 'city', 'crossfit_affiliate', 'country', 'state')
+    search_fields = ('name', 'city', 'country', 'state')
+
+
+admin.site.register(Affiliate, AffiliateAdmin)
