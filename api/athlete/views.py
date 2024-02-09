cdae0be0b27fdca28ceccf6b54239213674accb3
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2024-02-09 19:52:12 +0100

    update views file for athlete

--- api/athlete/views.py
+++ api/athlete/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 
 from api.athlete.serializers import AthleteSerializer
 from athlete.models import Athlete
-from drf_yasg import openapi
-from drf_yasg.utils import swagger_auto_schema
+# from drf_yasg import openapi
+# from drf_yasg.utils import swagger_auto_schema
 
 class AthleteViewSet(viewsets.ModelViewSet):
     queryset = Athlete.objects.all()
@@ -56,4 +56,4 @@ class AthleteViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
