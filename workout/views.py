637db9f608bfe1927699eb1fae7e1a62773e7126
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2024-02-13 19:18:20 +0100

    add endpoints for workout and workout component

--- workout/views.py
+++ workout/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from workout.models import Workout, WorkoutComponent
+from workout.serializers import WorkoutSerializer, WorkoutComponentSerializer
+
+
+class WorkoutViewSet(viewsets.ModelViewSet):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+    def get_queryset(self):
+        queryset = Workout.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name=name)
+        return queryset
+
+
+class WorkoutComponentViewSet(viewsets.ModelViewSet):
+    queryset = WorkoutComponent.objects.all()
+    serializer_class = WorkoutComponentSerializer
+
+    def get_queryset(self):
+        queryset = WorkoutComponent.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name=name)
+        return queryset
