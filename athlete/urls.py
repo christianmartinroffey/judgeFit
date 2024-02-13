1c22861bbe3c4fa755f3c2f2f3b72e136c842f2f
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2024-02-13 18:23:11 +0100

    add endpoints for competition

--- athlete/urls.py
+++ athlete/urls.py
@@ -5,6 +5,7 @@ from athlete import views
 router = routers.DefaultRouter()
 
 router.register(r'athlete', views.AthleteViewSet)
+router.register(r'competition', views.AthleteViewSet)
 
 urlpatterns = []
 urlpatterns += router.urls
--- athlete/views.py
+++ athlete/views.py
@@ -1,13 +1,25 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
-from athlete.serializers import AthleteSerializer
-from athlete.models import Athlete
+from athlete.serializers import AthleteSerializer, CompetitionSerializer
+from athlete.models import Athlete, Competition
 
 
 class AthleteViewSet(viewsets.ModelViewSet):
     queryset = Athlete.objects.all()
     serializer_class = AthleteSerializer
+
+    def get_queryset(self):
+        queryset = Athlete.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name=name)
+        return queryset
+
+
+class CompetitionViewSet(viewsets.ModelViewSet):
+    queryset = Competition.objects.all()
+    serializer_class = CompetitionSerializer
 
     def get_queryset(self):
         queryset = Athlete.objects.all()
