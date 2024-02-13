22d79be926d19b5b2877572bee42797b5f8d45b9
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2024-02-13 17:08:38 +0100

    add new models to admin

--- workout/admin.py
+++ workout/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Movement
+from .models import Movement, WorkoutComponent, Workout, Score
 
 
 class MovementAdmin(admin.ModelAdmin):
@@ -10,3 +10,30 @@ class MovementAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Movement, MovementAdmin)
+
+
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'total_reps', 'time_cap', 'is_active', 'is_scaled')
+    list_filter = ('name', 'type', 'is_active', 'is_scaled')
+    search_fields = ('name', 'description')
+
+
+admin.site.register(Workout, WorkoutAdmin)
+
+
+class WorkoutComponentAdmin(admin.ModelAdmin):
+    list_display = ('workout', 'movement', 'reps', 'sequence')
+    list_filter = ('workout', 'movement', 'reps', 'sequence')
+    search_fields = ('workout', 'movement')
+
+
+admin.site.register(WorkoutComponent, WorkoutComponentAdmin)
+
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('athlete', 'workout', 'score')
+    list_filter = ('athlete', 'workout')
+    search_fields = ('athlete', 'workout', 'competition')
+
+
+admin.site.register(Score, ScoreAdmin)
