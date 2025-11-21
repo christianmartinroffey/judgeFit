77ba3c536e768fc716de0f59a07c5bb4c6d021aa
christianroffey <c.martinroffey@gmail.com>
2025-11-21 21:22:20 +0100

    swt dummmy db in settings.py

--- judgeFit/settings.py
+++ judgeFit/settings.py
@@ -140,6 +140,18 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": env("POSTGRES_DB"),
+#         "USER": env("POSTGRES_USER"),
+#         "PASSWORD": env("POSTGRES_PASSWORD"),
+#         "HOST": env("POSTGRES_HOST"),
+#         "PORT": env("POSTGRES_PORT"),
+#         "OPTIONS": {"sslmode": "require"},
+#     }
+# }
+
 
 
 # Password validation
